6e3ac965795e08b68a8811f4a4066e61bcbf7f63
SHUBHAM YADAV <shubhamyadav.dev01@gmail.com>
2026-08-16 21:43:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -130,7 +130,7 @@ document.getElementById("enterBtn").addEventListener("click",async ()=>{
 });
 
 const cafeMusic = document.getElementById("cafeMusic");
-cafeMusic.volume = 0.05;
+cafeMusic.volume = 0.01;
 let musicOn = false;
 
 document.getElementById("soundBtn").addEventListener("click", async ()=>{
